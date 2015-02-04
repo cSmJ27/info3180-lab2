0b7ea18d531d41164be1e4fe441c4587c3958c58
cSmJ27 <christophe.s.james@gmail.com>
2015-02-04 20:58:37 +0000

    customized my app

--- app/views.py
+++ app/views.py
@@ -10,6 +10,8 @@ from app import app
 from flask import render_template, request, redirect, url_for
 
 
+import time
+
 ###
 # Routing for your application.
 ###
@@ -52,6 +54,17 @@ def add_header(response):
 def page_not_found(error):
     """Custom 404 page."""
     return render_template('404.html'), 404
+
+
+
+@app.route("/profile/")
+def profile():
+  return render_template('profile.html', time = timeinfo())
+
+def timeinfo():
+  now = time.strftime("%a, %d %b 20%y")
+  return now
+
 
 
 if __name__ == '__main__':
